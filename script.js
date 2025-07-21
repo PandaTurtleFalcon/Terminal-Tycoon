0bf6d01b42250252db425323e448cd8b06869e58
Timothy Chang <110138737+PandaTurtleFalcon@users.noreply.github.com>
2025-07-21 07:44:13 -0400

    added js for flask

--- script.js
+++ script.js
@@ -24,3 +24,21 @@ setInterval(() => {
     updateDisplay();
 
 }, 1000);
+function saveGame() {
+    fetch('/save', {
+        method: "POST",
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({loc,locPerClick })
+    });
+}
+function loadGame() {
+    fetch ('/load')
+        .then(res => res.json())
+        .then(data => {
+            loc = data.loc ?? 0;
+            locPerClick = data.locPerClick ?? 1;
+            updateDisplay();
+        });
+}
+window.onload = loadGame;
+setInterval(saveGame, 1000)
